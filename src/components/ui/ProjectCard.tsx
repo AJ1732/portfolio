22c367ea139edbf9535d1b462d5f708994c085e1
AJ1732 <ejemeniboi@gmail.com>
2024-06-28 00:39:01 +0100

    chore: add reverse functionality

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -1,51 +1,68 @@
-const ProjectCard = () => {
+import Reveal from "../anim/Reveal";
+
+interface Props {
+  reverse?: boolean;
+}
+const ProjectCard = ({ reverse }: Props) => {
   return (
-    <article className="flex size-full flex-col justify-between gap-8 md:flex-row">
-      <div className="max-w-96 space-y-4 lg:mt-8">
-        {/* PROJECT HEADER */}
-        <header className="flex items-center justify-between">
-          <h3 className="text-2xl">Project Title</h3>
-          <div className="rounded bg-primary-300/40 px-4 py-2 text-sm">
-            Type
-          </div>
-        </header>
+    <Reveal width="100%">
+      <article
+        className={`flex min-h-[calc(100dvh-8rem)] w-full flex-col items-center justify-between gap-8 ${reverse ? "md:flex-row-reverse" : "md:flex-row"}`}
+      >
+        <div className="max-w-96 space-y-4 lg:mt-8">
+          {/* PROJECT HEADER */}
+          <header className="flex items-center justify-between">
+            <h3 className="text-2xl">Project Title</h3>
+            <div className="rounded bg-primary-300/40 px-4 py-2 text-sm">
+              Type
+            </div>
+          </header>
 
-        {/* PROJECT DESCRIPTION */}
-        <p className="text-sm text-zinc-400">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae quod
-          in totam earum voluptatem ipsam?
-        </p>
+          {/* PROJECT DESCRIPTION */}
+          <p className={`text-sm text-zinc-400 ${reverse && "text-end"}`}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae
+            quod in totam earum voluptatem ipsam?
+          </p>
 
-        {/* PROJECT STACK */}
-        <div className="flex items-center justify-start gap-4 *:cursor-pointer *:rounded *:bg-neutral-800 *:px-3 *:py-1.5 *:text-sm">
-          <div className="transition-all duration-500 hover:bg-primary-400 hover:text-black">
-            ReactJS
+          {/* PROJECT STACK */}
+          <div
+            className={`flex pt-12 items-center ${reverse ? "justify-end" : "justify-start"} gap-4 *:cursor-pointer *:rounded *:bg-neutral-800 *:px-3 *:py-1.5 *:text-sm`}
+          >
+            <div className="transition-all duration-500 hover:bg-primary-400 hover:text-black">
+              ReactJS
+            </div>
+            <div className="transition-all duration-500 hover:bg-primary-400 hover:text-black">
+              TailwindCSS
+            </div>
+            <div className="transition-all duration-500 hover:bg-primary-400 hover:text-black">
+              Firebase
+            </div>
           </div>
-          <div className="transition-all duration-500 hover:bg-primary-400 hover:text-black">
-            TailwindCSS
-          </div>
-          <div className="transition-all duration-500 hover:bg-primary-400 hover:text-black">
-            Firebase
+
+          {/* PROJECT LINK */}
+          <div
+            className={`flex items-center ${reverse ? "justify-end" : "justify-start"} pl-1 gap-4 *:cursor-pointer *:py-1.5 *:text-sm`}
+          >
+            <div className="transition-all duration-500 hover:text-primary-400 hover:underline">
+              GitHub Repo
+            </div>
+            <div className="transition-all duration-500 hover:text-primary-400 hover:underline">
+              Live Link
+            </div>
           </div>
         </div>
 
-        {/* PROJECT LINK */}
-        <div className="flex items-center justify-start gap-4 *:cursor-pointer *:py-1.5 *:text-sm">
-          <div className="transition-all duration-500 hover:text-primary-400 hover:underline">
-            GitHub Repo
-          </div>
-          <div className="transition-all duration-500 hover:text-primary-400 hover:underline">
-            Live Link
-          </div>
-        </div>
-      </div>
-
-      {/* PROJECT IMAGE */}
-      <figure className="min-w-80">
-        <div className="size-60 rounded bg-primary-300"></div>
-        <div className="-mt-40 ml-auto size-60 rounded bg-primary-300 shadow-2xl"></div>
-      </figure>
-    </article>
+        {/* PROJECT IMAGE */}
+        <figure className="min-w-80">
+          <div
+            className={`size-60 rounded ${reverse ? "ml-auto" : "mr-auto"} bg-primary-300`}
+          ></div>
+          <div
+            className={`-mt-40 ${reverse ? "mr-auto" : "ml-auto"} size-60 rounded bg-primary-300 shadow-2xl`}
+          ></div>
+        </figure>
+      </article>
+    </Reveal>
   );
 };
 export default ProjectCard;
--- src/page/Projects/Page.tsx
+++ src/page/Projects/Page.tsx
@@ -3,8 +3,9 @@ import ProjectCard from "../../components/ui/ProjectCard";
 const ProjectsPage = () => {
   return (
     <main className="content-grid relative place-items-center py-12">
-      <div className="flex size-full flex-col gap-12">
+      <div className="flex size-full flex-col gap-12 lg:gap-20">
         <ProjectCard />
+        <ProjectCard reverse={true} />
       </div>
     </main>
   );
