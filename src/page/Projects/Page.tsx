770b7660658d2416f7c26603c2c403ff493a3a15
AJ1732 <ejemeniboi@gmail.com>
2024-06-28 01:01:47 +0100

    chore: import project data

--- src/page/Projects/Page.tsx
+++ src/page/Projects/Page.tsx
@@ -1,11 +1,17 @@
 import ProjectCard from "../../components/ui/ProjectCard";
+import { projects } from "../../data/db.json";
 
 const ProjectsPage = () => {
   return (
     <main className="content-grid relative place-items-center py-12">
       <div className="flex size-full flex-col gap-12 lg:gap-20">
-        <ProjectCard />
-        <ProjectCard reverse={true} />
+        {projects.map((project) => (
+          <ProjectCard
+            key={project.id}
+            reverse={project.id % 2 === 0}
+            {...project}
+          />
+        ))}
       </div>
     </main>
   );
