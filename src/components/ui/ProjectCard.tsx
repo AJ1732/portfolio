9cf1e4f5cd19a28e0ced72810934a601ded50257
AJ1732 <ejemeniboi@gmail.com>
2024-06-28 01:14:41 +0100

    chore: utilize project data to card component

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -1,9 +1,23 @@
 import Reveal from "../anim/Reveal";
 
 interface Props {
+  title: string;
+  type: string;
+  desc: string;
+  stack: string[];
+  src1: string;
+  src2: string;
   reverse?: boolean;
 }
-const ProjectCard = ({ reverse }: Props) => {
+const ProjectCard = ({
+  title,
+  type,
+  desc,
+  stack,
+  src1,
+  src2,
+  reverse,
+}: Props) => {
   return (
     <Reveal width="100%">
       <article
@@ -12,36 +26,34 @@ const ProjectCard = ({ reverse }: Props) => {
         <div className="max-w-96 space-y-4 lg:mt-8">
           {/* PROJECT HEADER */}
           <header className="flex items-center justify-between">
-            <h3 className="text-2xl">Project Title</h3>
+            <h3 className="text-2xl">Project {title}</h3>
             <div className="rounded bg-primary-300/40 px-4 py-2 text-sm">
-              Type
+              {type}
             </div>
           </header>
 
           {/* PROJECT DESCRIPTION */}
           <p className={`text-sm text-zinc-400 ${reverse && "text-end"}`}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae
-            quod in totam earum voluptatem ipsam?
+            {desc}
           </p>
 
           {/* PROJECT STACK */}
           <div
-            className={`flex pt-12 items-center ${reverse ? "justify-end" : "justify-start"} gap-4 *:cursor-pointer *:rounded *:bg-neutral-800 *:px-3 *:py-1.5 *:text-sm`}
+            className={`flex flex-wrap items-center pt-12 ${reverse ? "justify-end" : "justify-start"} gap-2 *:cursor-pointer *:rounded *:bg-neutral-800 *:px-3 *:py-1.5 *:text-sm`}
           >
-            <div className="transition-all duration-500 hover:bg-primary-400 hover:text-black">
-              ReactJS
-            </div>
-            <div className="transition-all duration-500 hover:bg-primary-400 hover:text-black">
-              TailwindCSS
-            </div>
-            <div className="transition-all duration-500 hover:bg-primary-400 hover:text-black">
-              Firebase
-            </div>
+            {stack.map((stac) => (
+              <div
+                key={stac}
+                className="transition-all duration-500 hover:bg-primary-400 hover:text-black"
+              >
+                {stac}
+              </div>
+            ))}
           </div>
 
           {/* PROJECT LINK */}
           <div
-            className={`flex items-center ${reverse ? "justify-end" : "justify-start"} pl-1 gap-4 *:cursor-pointer *:py-1.5 *:text-sm`}
+            className={`flex items-center ${reverse ? "justify-end" : "justify-start"} gap-4 pl-1 *:cursor-pointer *:py-1.5 *:text-sm`}
           >
             <div className="transition-all duration-500 hover:text-primary-400 hover:underline">
               GitHub Repo
@@ -54,12 +66,16 @@ const ProjectCard = ({ reverse }: Props) => {
 
         {/* PROJECT IMAGE */}
         <figure className="min-w-80">
-          <div
+          <img
+            src={src1}
             className={`size-60 rounded ${reverse ? "ml-auto" : "mr-auto"} bg-primary-300`}
-          ></div>
-          <div
+            alt="Project Image 1"
+          />
+          <img
+            src={src2}
             className={`-mt-40 ${reverse ? "mr-auto" : "ml-auto"} size-60 rounded bg-primary-300 shadow-2xl`}
-          ></div>
+            alt="Project Image 2"
+          />
         </figure>
       </article>
     </Reveal>
