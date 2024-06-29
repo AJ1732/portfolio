7c277d8f20621196ba07cd451b2f4f1f923a5d38
AJ1732 <ejemeniboi@gmail.com>
2024-06-29 09:56:08 +0100

    chore: change animation

--- src/components/anim/ShinyButton.tsx
+++ src/components/anim/ShinyButton.tsx
@@ -11,33 +11,33 @@ interface Props {
 const ShinyButton = ({ children, className }: Props) => {
   return (
     <motion.button
-      initial={{ "--x": "100%", scale: 1 }}
-      animate={{ "--x": "-100%" }}
-      whileTap={{ scale: 0.97 }}
-      transition={{
-        repeat: Infinity,
-        repeatType: "loop",
-        repeatDelay: 1,
-        type: "spring",
-        stiffness: 20,
-        damping: 15,
-        mass: 2,
-        scale: {
-          type: "spring",
-          stiffness: 10,
-          damping: 5,
-          mass: 0.1,
-        },
-      }}
+      // initial={{ "--x": "100%", scale: 1 }}
+      // animate={{ "--x": "-100%" }}
+      // whileTap={{ scale: 0.97 }}
+      // transition={{
+      //   repeat: Infinity,
+      //   repeatType: "loop",
+      //   repeatDelay: 1,
+      //   type: "spring",
+      //   stiffness: 20,
+      //   damping: 15,
+      //   mass: 2,
+      //   scale: {
+      //     type: "spring",
+      //     stiffness: 10,
+      //     damping: 5,
+      //     mass: 0.1,
+      //   },
+      // }}
       className={twMerge(
         "radial-gradient relative rounded-md px-6 py-2 transition-all duration-300 active:scale-95 active:shadow active:shadow-neutral-600",
         className,
       )}
     >
-      <span className="linear-mask relative block size-full font-light tracking-wide text-neutral-100">
+      <span className="linear-mask shinyAnimation relative block size-full font-light tracking-wide text-neutral-100">
         {children}
       </span>
-      <span className="linear-overlay absolute inset-0 block rounded-md p-px"></span>
+      <span className="linear-overlay shinyAnimation absolute inset-0 block rounded-md p-px"></span>
     </motion.button>
   );
 };
