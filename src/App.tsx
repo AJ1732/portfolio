27d53b33d358f43a8f8d1125ba784d7ee97d6bb7
AJ1732 <ejemeniboi@gmail.com>
2024-06-29 05:38:14 +0100

    chore: import shiny button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import cv from "./assets/OSEZELE_EJEMEN_IBOI_FRONTEND_ENGINEER.pdf";
+import ShinyButton from "./components/anim/ShinyButton";
 
 function App() {
   return (
     <div className="min-h-dvh font-poppins">
       {/* CV DOWNLOAD */}
-      <button className="fixed right-8 top-11 z-50 rounded bg-primary-400/70 px-4 py-2 transition-all duration-500 ease-in-out hover:bg-primary-400/100">
+      <ShinyButton className="fixed right-8 top-11 z-50">
         <a href={cv} download role="button">
           Resume
         </a>
-      </button>
+      </ShinyButton>
 
       {/* SIDE NAVIGATION */}
       <div className="fixed h-dvh max-w-[4.5rem] md:max-w-20">
