7cacc1d55deccc200f081bbcc2cb97d39fc1d1a7
AJ1732 <ejemeniboi@gmail.com>
2024-06-29 04:08:18 +0100

    chore: add comments and resume button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
+import cv from "./assets/OSEZELE_EJEMEN_IBOI_FRONTEND_ENGINEER.pdf";
 
 function App() {
   return (
-    <div className="font-poppins min-h-dvh">
+    <div className="min-h-dvh font-poppins">
+      {/* CV DOWNLOAD */}
+      <button className="fixed right-8 top-11 z-50 rounded bg-primary-400/70 px-4 py-2 transition-all duration-500 ease-in-out hover:bg-primary-400/100">
+        <a href={cv} download role="button">
+          Resume
+        </a>
+      </button>
+
+      {/* SIDE NAVIGATION */}
       <div className="fixed h-dvh max-w-[4.5rem] md:max-w-20">
         <Sidebar />
       </div>
 
+      {/* MAIN CONTENT */}
       <div className="ml-[4.5rem] md:ml-20">
         <Outlet />
       </div>
