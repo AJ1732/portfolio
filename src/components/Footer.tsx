a26b7046fc4bb13abd7557117ddafeea0efa0e05
AJ1732 <ejemeniboi@gmail.com>
2024-08-13 20:00:54 +0100

    chore: update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,10 +5,15 @@ const Footer = () => {
         className="full-width relative h-[400px]"
         style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
       >
-        <div className="fixed bottom-0 px-4 flex h-[400px] w-full items-end justify-start py-8 font-thin">
-          <h2 className="text-[clamp(1.2rem,7vw,6.5rem)] hover:text-neutral-950 decoration-1 hover:underline">
+        <div className="fixed bottom-0 flex h-[400px] w-full items-end justify-start px-4 py-8 font-thin">
+          <h2 className="text-[clamp(1.2rem,7vw,6.5rem)]">
             Email:{" "}
-            <a href="mailto: ejemeniboi@gmail.com"> ejemeniboi@gmail.com </a>
+            <a
+              href="mailto: ejemeniboi@gmail.com"
+              className="decoration-1 transition-all duration-300 hover:text-blue-950 hover:underline"
+            >
+              ejemeniboi@gmail.com{" "}
+            </a>
           </h2>
         </div>
       </div>
